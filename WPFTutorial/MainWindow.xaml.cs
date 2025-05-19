f41b8539fc85c9228f53c12e2b0b8e181e3c8406
Onur <onurdogruiletisim@gmail.com>
2025-05-20 00:35:38 +0300

    #4

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Net.NetworkInformation;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,32 @@ namespace WPFTutorial
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool running = false;
         public MainWindow()
         {
             InitializeComponent();
+
+
+            //tbHello.Text = "welcome my planet";
+            //btnRun.Content = "Stop";
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                //run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            
+            running = !running;
         }
     }
 }
